fe78bcaa20fb089566f3d9c228984a1dcdc379c2
Ming-Jung YU <minayu416@gmail.com>
2022-11-14 22:41:52 +0800

    add dist and generate settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,44 @@
+function toYearName(date) {
+  const options = { year: "numeric", };
+  return new Date(date).toLocaleDateString("en", options);
+}
+
+function toMonthRoute(date) {
+  const options = { month: "numeric", };
+  const num = new Date(date).toLocaleDateString("en", options);
+  return (num < 10) ? '0' + num.toString() : num.toString();
+}
+
+const createRoutes = async () => {
+    const { $content } = require('@nuxt/content')
+    const posts = await $content("posts").only(['slug', 'date', 'categories']).where({ status: { $eq: "published" } }).fetch();
+    const pageNumber = Math.ceil(posts.length/16);
+    const categorySet = new Set();
+    const workRoutes = []
+    // Get category list
+    posts.forEach(post => {
+    categorySet.add(post.categories[0])
+    categorySet.add(post.categories[1])
+    })
+    // Get all posts routes
+    posts.forEach(post => {
+      workRoutes.push("/" + toYearName(post.date) + "/" + toMonthRoute(post.date) + "/" + post.slug)
+      workRoutes.push("/en/" + toYearName(post.date) + "/" + toMonthRoute(post.date) + "/" + post.slug)
+    })
+    // Get all category routes
+    categorySet.forEach(category => {
+      workRoutes.push("/blog/category/" + category)
+      workRoutes.push("/en/blog/category/" +category)
+    })
+    // Get all pages
+    for (let page = 2; page <= pageNumber; page++) {
+      workRoutes.push("/blog/page/" + page)
+      workRoutes.push("/en/blog/page/" + page)
+    }
+    // workRoutes.push("/2022/1/human-design-g-center-not-define-2")
+    return workRoutes
+}
+
 const seoSetting = {}
 seoSetting.title = "Own Influence Creative Theme"
 seoSetting.description = "A creative front-end design theme by Mina."
@@ -8,6 +49,15 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
+  router:{
+    // Staging setting
+    // base: '/staging/'
+    base: '/'
+    },
+    generate: {
+      fallback: true,
+      routes: createRoutes
+    },
   head: {
     title: seoSetting.title,
     htmlAttrs: {
